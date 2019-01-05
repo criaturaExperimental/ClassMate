1f625660b100c4a2aaeae43a6b4e0c4bbbd99feb
Lucas deGomez <lucasdegomez@gmail.com>
2019-01-05 13:42:13 +0100

    Adds unique classes count feature

--- src/extension.ts
+++ src/extension.ts
@@ -4,16 +4,19 @@ export function activate(context: ExtensionContext) {
 
     let classSeeker = new ClassSeeker();
 
-    let showUniqueClasses = commands.registerCommand('extension.showUnique', () => {
-        const classesArray = classSeeker.updateClasses(true);
-        displayList(classesArray, 'unique');
-        console.log('Time to get unique and classy!');
+    let showAllClasses = commands.registerCommand('extension.showAll', () => {
+        const classesArray = classSeeker.getAllClasses();
+        displayList(classesArray, 'total');
     });
 
-    let showAllClasses = commands.registerCommand('extension.showAll', () => {
-        const classesArray = classSeeker.updateClasses(false);
-        displayList(classesArray, 'total');
-        console.log('Time to get classy!');
+    let showAllClassesSorted = commands.registerCommand('extension.showAllSorted', () => {
+        const classesArray = classSeeker.getAllClassesSorted();
+        displayList(classesArray, 'total (sorted)');
+    })
+
+    let showUniqueClasses = commands.registerCommand('extension.showUnique', () => {
+        let classesFrequency = classSeeker.getUniqueClasses();
+        displayFrequency(classesFrequency, 'unique');
     });
 
     function displayList(classes: Array<string>, message: String) {
@@ -23,16 +26,84 @@ export function activate(context: ExtensionContext) {
         outputPanel.appendLine(classes.join('\n'));
         outputPanel.appendLine(output);
         outputPanel.show(true);
-        console.log('Time to get totally classy!');
+    }
+
+    function displayFrequency(pairArray: String[][], message: String) {
+        let outputPanel = window.createOutputChannel("HTML classes");
+        const output = `There are ${pairArray.length} ${message} classes present in your document:`
+        outputPanel.appendLine(output);
+        pairArray.forEach(element => {
+            outputPanel.appendLine(element.join(' '))
+        })
+        outputPanel.appendLine(output);
+        outputPanel.show(true);
     }
 
     // Add to a list of disposables which are disposed when this extension is deactivated.
     context.subscriptions.push(classSeeker);
     context.subscriptions.push(showUniqueClasses);
     context.subscriptions.push(showAllClasses);
+    context.subscriptions.push(showAllClassesSorted);
 }
 
 class ClassSeeker {
+
+    public getAllClasses() {
+        return this.searchClassesInText(this.getTextDocument());
+    }
+
+    public getAllClassesSorted() {
+        return this.getAllClasses().sort();
+    }
+
+    public getUniqueClasses() {
+        return this.arrayFrequency(this.getAllClassesSorted());
+    }
+
+    private arrayFrequency(array: any[]) {
+        const output = array.reduce(function (acc, curr) {
+            if (typeof acc[curr] == 'undefined') {
+              acc[curr] = 1;
+            } else {
+              acc[curr] += 1;
+            }
+            return acc;
+          }, {});
+        return this.frequencyToArray(output);
+    }
+
+    private frequencyToArray(obj: Object) {
+        let array = Object.keys(obj).map(function(key) {
+            return [key, `[${String(obj[key])}]`];
+        });
+        return array;
+    }
+
+    private getTextDocument() {
+        // Get the current text editor
+        let editor = window.activeTextEditor;
+        if (!editor) {
+            console.log('no file in editor');
+        } else {
+            return editor.document.getText();
+        }
+    }
+
+    private searchClassesInText(rawText: String) {
+        const patternClass = /class="([^":{}]+)"/g;
+        const patternClassName = /"((?:\\.|[^"\\])*)"/g;
+
+        let classTags = rawText.match(patternClass);
+        let allClassesArray = [];
+
+        classTags.forEach( item => {
+            let input;
+            input = item.match(patternClassName)[0].replace(/"/g, "").split(" ");
+            allClassesArray = allClassesArray.concat(input);
+        });
+        return allClassesArray;
+    }
+
     public updateClasses(unique: Boolean) {
         // Get the current text editor
         let editor = window.activeTextEditor;
